ab120d91ae00c6314cb8b23c451960a1261f34b9
PERSONAL <PERSONAL@DESKTOP-S5IPCQP>
2024-05-30 08:04:59 -0500

    EJERCICIO_COMPETENCIA_50%

---
+++ src/Programa3.java
@@ -0,0 +1,13 @@
+
+public class Programa3 {
+   public static void main(String arg[])
+        {
+              String nombre[]={"Juan","Pedro","Luis","Lina","Diana"};
+              for(String x: nombre)
+            {
+                  System.out.println(x);
+
+              }
+        }
+ 
+}
